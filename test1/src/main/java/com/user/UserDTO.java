c9a2f373f39e36e7c78156c2f63bd1781f850453
jsooo <jsooo@DESKTOP-3L7N7L8>
2022-04-25 15:05:04 +0900

    First Test

---
+++ test1/src/main/java/com/user/UserDTO.java
@@ -0,0 +1,13 @@
+package com.user;
+
+public class UserDTO {
+	private String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+}
